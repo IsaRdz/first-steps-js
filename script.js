c52fd677b216a23d4ea0447b830a91a1f12724c5
IsaRdz <87336052+IsaRdz@users.noreply.github.com>
2022-10-14 18:14:26 -0300

    :bug: Fix name of type object

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 //type: String
-const firstName = "Isabel"; 
+const firstName = "Isabel";
+
 //type: String
-const lastName = "Rodriguez"; 
+const lastName = "Rodriguez";
+
 //type: number
-var age = 34; 
+var age = 34;
+
 //type: String
-const gender = "female"; 
+const gender = "female";
+
 //type: String
-const birthDate = "24/12/87"; 
+const birthDate = "24/12/87";
+
 //type: boolean
-var isMarried = false; 
+var isMarried = false;
+
 //type: boolean
-var couple = true; 
+var couple = true;
+
 //type: String
-var country = "Argentina"; 
+var country = "Argentina";
+
 //type: String
-var city = "El Cadillal"; 
-//type: boolean
+var city = "El Cadillal";
+
+//type: Object
 var pet = {
-    petName : "Nerón",
-    petAge : 5,
-    breed : "Bullmastiff",
-    hobbies : ["run", "sleep", "eat", "watch neighbors"],
-    weight : 45
-} 
+  petName: "Nerón",
+  petAge: 5,
+  breed: "Bullmastiff",
+  hobbies: ["run", "sleep", "eat", "watch neighbors"],
+  weight: 45,
+};
 console.log(`Hi! My name is ${firstName} ${lastName}. I'm ${gender}. 
 I was born on ${birthDate}, so I'm ${age} years old. I live in ${city}, ${country}. 
 My pet's name is ${pet.petName}, he is ${pet.petAge} years old, his breed is ${pet.breed} and he loves ${pet.hobbies}`);
